d22813089f8f17ab32b487255d5c4fa98fb9ad63
Madalina <cristina.madalina.dragan@gmail.com>
2016-11-15 20:56:18 +0100

    added usage of the Bank class (commented out)

--- RuleeBasedWS/src/main/java/ruleBaseWS/RuleBaseResponse.java
+++ RuleeBasedWS/src/main/java/ruleBaseWS/RuleBaseResponse.java
@@ -3,31 +3,31 @@ package ruleBaseWS;
 import java.util.ArrayList;
 
 public class RuleBaseResponse {
-//    private ArrayList<String> relevantBanks;
-//
-//    public RuleBaseResponse( ArrayList<String> relevantBanks ) {
-//        this.relevantBanks = relevantBanks;
-//    }
-//
-//    public ArrayList<String> getRelevantBanks() {
-//        return relevantBanks;
-//    }
-//
-//    public void setRelevantBanks( ArrayList<String> relevantBanks ) {
-//        this.relevantBanks = relevantBanks;
-//    }
+    private ArrayList<String> relevantBanks;
 
-    private ArrayList<Bank> relevantBanks;
-
-    public RuleBaseResponse( ArrayList<Bank> relevantBanks ) {
+    public RuleBaseResponse( ArrayList<String> relevantBanks ) {
         this.relevantBanks = relevantBanks;
     }
 
-    public ArrayList<Bank> getRelevantBanks() {
+    public ArrayList<String> getRelevantBanks() {
         return relevantBanks;
     }
 
-    public void setRelevantBanks( ArrayList<Bank> relevantBanks ) {
+    public void setRelevantBanks( ArrayList<String> relevantBanks ) {
         this.relevantBanks = relevantBanks;
     }
+
+//    private ArrayList<Bank> relevantBanks;
+//
+//    public RuleBaseResponse( ArrayList<Bank> relevantBanks ) {
+//        this.relevantBanks = relevantBanks;
+//    }
+//
+//    public ArrayList<Bank> getRelevantBanks() {
+//        return relevantBanks;
+//    }
+//
+//    public void setRelevantBanks( ArrayList<Bank> relevantBanks ) {
+//        this.relevantBanks = relevantBanks;
+//    }
 }
